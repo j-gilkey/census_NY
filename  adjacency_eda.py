2847923225b171e6cccacd76415af146ccfb9e3f
“jackson_gilkey” <jackson.gilkey@gmail.com>
2020-05-03 16:26:29 -0400

    added graph plotting function and fixed a filtering issue

---  adjacency_eda.py
+++  adjacency_eda.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import percent_public_trasnpo
 import networkx as nx
+import matplotlib.pyplot as plt
 
 def process_tuples(tuple_input):
     final_tuples = []
@@ -8,9 +9,16 @@ def process_tuples(tuple_input):
     for tuple in tuple_input:
         if (tuple[1],tuple[0]) not in final_tuples:
             if tuple[1] != tuple[0]:
-                final_tuples.append(tuple)
+                final_tuples.append((str(tuple[0]),str(tuple[1])))
 
     return final_tuples
+
+def draw_graph(G):
+    plt.close()
+    #plt.subplot(121)
+    nx.draw(G, with_labels=True, font_weight='bold')
+    plt.show()
+    return
 
 
 def adjacency_graph():
@@ -23,21 +31,23 @@ def adjacency_graph():
     adj_df = pd.read_csv('county_adjacency2010.csv')
     #read in adjancency data
     adj_df = adj_df[adj_df['fipscounty'].isin(county_FIPS)]
+    adj_df = adj_df[adj_df['fipsneighbor'].isin(county_FIPS)]
     #filter down to only NY counties
     adj_df = adj_df[['fipscounty', 'fipsneighbor']]
     #remove all columns but FIPS
     fips_tuples = list(adj_df.itertuples(index=False, name=None))
-    print(len(fips_tuples))
     fips_tuples = process_tuples(fips_tuples)
-    print(len(fips_tuples))
 
     G = nx.Graph()
     G.add_nodes_from(county_FIPS)
+    G.add_edges_from(fips_tuples)
     #instantiate graph and add nodes for each county FIPS
 
+    draw_graph(G)
 
-    print(adj_df.head)
-    print(fips_tuples)
+
+    #print(adj_df.head)
+    #print(fips_tuples)
     #print(df.head)
 
 adjacency_graph()
