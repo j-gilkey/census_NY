6d64d67b17097ded0606d1b100e8458a034e7a3c
“jackson_gilkey” <jackson.gilkey@gmail.com>
2020-05-03 13:51:59 -0400

    refactored and commented exisiting code

---  adjacency_eda.py
+++  adjacency_eda.py
@@ -1,11 +1,26 @@
 import pandas as pd
+import percent_public_trasnpo
+import networkx as nx
 
 
 def adjacency_graph():
+
+    df = percent_public_trasnpo.create_dataframe()
+    #get ny transpo census data
+    county_FIPS = list(df.index)
+    #make a list of each county
+
     adj_df = pd.read_csv('county_adjacency2010.csv')
+    #read in adjancency data
+    adj_df = adj_df[adj_df['fipscounty'].isin(county_FIPS)]
+    #filter down to only NY counties
 
-    df = censusdata.download('acs5', 2015,
-                                 censusdata.censusgeo([('state', '36'), ('county', '*')]),
-                                 ['B08301_001E', 'B08301_010E'])
+    G = nx.Graph()
+    G.add_nodes_from(county_FIPS)
+    #instantiate graph and add nodes for each county FIPS
 
-print(df.head)
+
+    print(adj_df.head)
+    #print(df.head)
+
+adjacency_graph()
