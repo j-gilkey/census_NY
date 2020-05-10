efbeb8e8a70fe4eb3b2208494d19b281740cfe0d
“jackson_gilkey” <jackson.gilkey@gmail.com>
2020-05-10 19:50:59 -0400

    cleaned up adjaceny_eda

---  adjacency_eda.py
+++  adjacency_eda.py
@@ -4,12 +4,16 @@ import networkx as nx
 from tabulate import tabulate
 import matplotlib.pyplot as plt
 
-def process_tuples(df):
+def filter_tuples(df):
+    #takes in an adjancency df and filters out symetrical and self adjacencies
+    #this was counties won't have two edges between eachother
     all_tuples = list(df.itertuples(index=False, name=None))
     final_tuples = []
 
     for tuple in all_tuples:
+        #check for symetry
         if (tuple[1],tuple[0]) not in final_tuples:
+            #check for self-adjacencies
             if tuple[1] != tuple[0]:
                 final_tuples.append((str(tuple[0]),str(tuple[1])))
 
@@ -20,13 +24,11 @@ def process_counties(df):
     for index, row in df.iterrows():
         node = (str(index), {'percent_public_transpo': row['percent_public_transpo'], 'county_name': row['county_name']})
         final_list.append(node)
-    #print(final_list)
     return final_list
 
 
 def draw_graph(G):
     plt.close()
-    #plt.subplot(121)
     nx.draw(G, with_labels=True, font_weight='bold')
     plt.show()
     return
@@ -41,27 +43,20 @@ def adjacency_graph():
     #make a list of each county
 
     adj_df = pd.read_csv('county_adjacency2010.csv')
-    #print(tabulate(adj_df,headers="keys"))
-    #return
     #read in adjancency data
     adj_df = adj_df[adj_df['fipscounty'].isin(county_FIPS_list)]
     adj_df = adj_df[adj_df['fipsneighbor'].isin(county_FIPS_list)]
     #filter down to only NY counties
     adj_df = adj_df[['fipscounty', 'fipsneighbor']]
     #remove all columns but FIPS
-    fips_tuples = process_tuples(adj_df)
+    fips_tuples = filter_tuples(adj_df)
 
     G = nx.Graph()
     G.add_nodes_from(county_FIPS)
-    #print(G.nodes)
     G.add_edges_from(fips_tuples)
     #instantiate graph and add nodes for each county FIPS
 
     draw_graph(G)
 
-
-    #print(adj_df.head)
-    #print(fips_tuples)
-    #print(df.head)
 
 adjacency_graph()
