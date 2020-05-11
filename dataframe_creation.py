697af00a946435ea8fa2bb543b3e2d46a0483e84
“jackson_gilkey” <jackson.gilkey@gmail.com>
2020-05-10 20:57:59 -0400

    removed eda file and commented dataframe creation

--- dataframe_creation.py
+++ dataframe_creation.py
@@ -8,17 +8,24 @@ pd.set_option('display.precision', 2)
 
 def create_dataframe():
     column_names = ['total_workforce', 'total_unemployed', 'total_transpo', 'drove_alone', 'carpooled', 'total_public_transpo', 'bus', 'streetcar', 'subway', 'railroad', 'ferry', 'taxi', 'motorcycle', 'bike','walked', 'other', 'wfh']
+    #descriptive column names to
 
     df = censusdata.download('acs5', 2015,
                                  censusdata.censusgeo([('state', '36'), ('county', '*')]),
                                  ['B23025_002E', 'B23025_005E', 'B08301_001E', 'B08301_003E', 'B08301_004E', 'B08301_010E', 'B08301_011E', 'B08301_012E', 'B08301_013E','B08301_014E', 'B08301_015E',
                                     'B08301_016E', 'B08301_017E', 'B08301_018E','B08301_019E', 'B08301_020E', 'B08301_021E'])
+    #uses the censusdata package to download census information
+    #the state numbers is a FIPS code that indicates New York
+    #the * in county indicates all counties should be selected
+    #the list of codes like 'B23025_002E' refers to specfic tables of census date that were looked up beforehand
 
 
     df = pd.DataFrame(df)
     df.columns = column_names
     df['percent_unemployed'] = df.apply(lambda row: row['total_unemployed']/row['total_workforce'] , axis = 1)
+    #create percentage unemployment column
     df['percent_public_transpo'] = df.apply(lambda row: row['total_public_transpo']/row['total_transpo'] , axis = 1)
+    #create percentage public transit use
 
     index_list = df.index.tolist()
     new_indices = []
@@ -32,10 +39,4 @@ def create_dataframe():
         county_names.append(county_name)
 
 
-    print(new_indices)
-    print(county_names)
-
-    #for county in index_list:
-
-
-create_dataframe()
+#create_dataframe()
--- eda.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import seaborn as sns
-import numpy as np
-import matplotlib.pyplot as plt
